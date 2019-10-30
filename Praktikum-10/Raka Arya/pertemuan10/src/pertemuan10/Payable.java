e169a300947267adf0d7b9958ff70fc731d5f6da
RakaArya07 <56916796+RakaArya07@users.noreply.github.com>
2019-10-30 17:17:02 +0700

    Pertemuan 10

---
+++ Praktikum-10/Raka Arya/pertemuan10/src/pertemuan10/Payable.java
@@ -0,0 +1,5 @@
+package pertemuan10;
+
+public interface Payable {
+    public int getPaymentAmount();
+}
---
+++ Praktikum-10/Raka Arya/pertemuan10/src/pertemuan10/Pertemuan10.java
@@ -0,0 +1,9 @@
+package pertemuan10;
+
+public class Pertemuan10 {
+
+    public static void main(String[] args) {
+  
+    }
+    
+}
